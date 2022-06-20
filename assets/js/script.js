3241b593a5a5aba970193611b5fc75a932f08252
Hamayoun <hamayounemail@gmail.com>
2022-06-20 16:57:44 +0430

    Create mobile menu
    1. Updated style.css file
    2. Updated index.html file
    3. Created script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function myFunction2(x) {
+  x.classList.toggle("change");
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
