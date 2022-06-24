c8dd9244fc747dded6df89bc8b68e9bfdfd56641
Hamayoun <hamayounemail@gmail.com>
2022-06-24 10:54:25 +0430

    Portfolio details popup window
    1. Updated index.html file
    2. Updated style.css file
    3. Updated script.js file
    4. Created popup.js file

---
+++ assets/js/index.js
@@ -0,0 +1,73 @@
+
+// Project section contents
+let arr = [
+'Data Dashboard Health Care',
+'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+'HTML',
+'BootStrap',
+'Ruby',
+'See Live',
+'See Source'
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+  AddNew();
+});
+
+let DivContainer;
+for(i = 1; i < 12; i ++) {
+    window['DivContainer'+i] = document.getElementById('cards-sample-'+i);
+}
+
+function AddNew(){
+ 
+for(let j = 1; j < 12; j ++){
+    const newH1 = document.createElement('h1');
+    newH1.insertAdjacentText('beforeend', arr[0]);
+    newH1.classList.add('work-sample');
+    window['DivContainer'+j].appendChild(newH1);
+
+    const newP = document.createElement('p');
+    newP.insertAdjacentText('beforeend', arr[1]);
+    newP.classList.add('work-sample-text');
+    window['DivContainer'+j].appendChild(newP); 
+
+    for(let i = 0; i < 3; i ++){
+      const newLi = document.createElement('li');
+      newLi.insertAdjacentText('beforeend', arr[i +2]);
+      newLi.classList.add('tag');
+      window['DivContainer'+j].appendChild(newLi);
+    }
+  }
+}
+
+// Mobile Menu Portions
+function toggleButton(a) {
+  a.classList.toggle('change');
+}
+
+toggleButton();
+
+function myFunction() {
+  const x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
+  }
+  const y = document.getElementById('brand');
+  if (y.className === 'hide-2') {
+    y.className = 'hide';
+  } else {
+    y.className = 'hide-2';
+  }
+}
+
+myFunction();
+
+function hideMenu() {
+  const z = document.getElementById('myTopnav');
+  z.className = 'topnav';
+}
+
+hideMenu();
---
+++ assets/js/popup.js
@@ -0,0 +1,72 @@
+var popup = document.getElementById('myPopup');
+var span = document.getElementsByClassName("close")[0];
+
+let arr2 = [
+'Data Dashboard Health Care',
+'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+'HTML',
+'BootStrap',
+'Ruby',
+'See Live',
+'See Source',
+'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry Ipsum is simply dummy text of the printing and typesetting industry.'
+];
+
+let DivContainer2 = document.getElementById('div-container');
+let DivContainer3 = document.getElementById('div-container2');
+
+function myFunction(elem) {
+	popup.style.display = "block";
+	popupContent();
+	popupContent2();
+}
+
+function popupContent() {
+	 const newH1 = document.createElement('h1');
+    newH1.insertAdjacentText('beforeend', arr2[0]);
+    newH1.classList.add('work-sample-popup');
+    DivContainer2.appendChild(newH1);
+    
+    for(let i = 0; i < 3; i ++){
+      const newLi = document.createElement('li');
+      newLi.insertAdjacentText('beforeend', arr2[i +2]);
+      newLi.classList.add('tag-popup');
+      DivContainer2.appendChild(newLi);
+    }
+
+    const img = document.createElement("img"); 
+		img.src = "assets/images/popup.png"; 
+		img.classList.add('img-popup');
+		const src = document.getElementById("div-container"); 
+		 
+		src.appendChild(img); 
+
+}
+
+function popupContent2() {
+  const newP = document.createElement('p');
+  newP.insertAdjacentText('beforeend', arr2[7]);
+  newP.classList.add('work-sample-text-popup');
+  DivContainer3.appendChild(newP); 
+
+  const img3 = document.createElement("img"); 
+	img3.src = "assets/images/Enable2.png"; 
+	img3.classList.add('tag-popup-2');
+	DivContainer3.appendChild(img3);
+
+  const img2 = document.createElement("img"); 
+	img2.src = "assets/images/source2.png"; 
+	img2.classList.add('tag-popup-2');
+	DivContainer3.appendChild(img2);    
+}
+
+span.onclick = function() {
+  popup.style.display = "none";
+  window.location.reload();
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    popup.style.display = "none";
+  }
+}
