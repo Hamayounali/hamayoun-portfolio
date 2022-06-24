5472047bdc52b596a9d9570ac2b2532c1815ca33
Hamayoun <hamayounemail@gmail.com>
2022-06-24 15:03:20 +0430

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,30 +10,30 @@ const arr = [
 ];
 
 const DivContainer = null;
-for (let i = 1; i < 12; i +=1) {
+for (let i = 1; i < 12; i += 1) {
   window[`DivContainer${i}`] = document.getElementById('cards-sample-'+i);
 }
 
 function AddNew(){
  
-for (let j = 1 ; j < 12; j +=1) {
-  const newH1 = document.createElement('h1');
-  newH1.insertAdjacentText('beforeend', arr[0]);
-  newH1.classList.add('work-sample');
-  window[`DivContainer${j}`].appendChild(newH1);
+for (let j = 1 ; j < 12; j += 1) {
+    const newH1 = document.createElement('h1');
+    newH1.insertAdjacentText('beforeend', arr[0]);
+    newH1.classList.add('work-sample');
+    window[`DivContainer${j}`].appendChild(newH1);
 
-  const newP = document.createElement('p');
-  newP.insertAdjacentText('beforeend', arr[1]);
-  newP.classList.add('work-sample-text');
-  window[`DivContainer${j}`].appendChild(newP); 
+    const newP = document.createElement('p');
+    newP.insertAdjacentText('beforeend', arr[1]);
+    newP.classList.add('work-sample-text');
+    window[`DivContainer${j}`].appendChild(newP); 
 
-  for (let i = 0; i < 3; i +=1) {
-    const newLi = document.createElement('li');
-    newLi.insertAdjacentText('beforeend', arr[i + 2]);
-    newLi.classList.add('tag');
-    window[`DivContainer${j}`].appendChild(newLi);
+    for (let i = 0; i < 3; i += 1) {
+      const newLi = document.createElement('li');
+      newLi.insertAdjacentText('beforeend', arr[i + 2]);
+      newLi.classList.add('tag');
+      window[`DivContainer${j}`].appendChild(newLi);
+    }
   }
-}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
